c4550bef6c9e8a994632ebeaf0de9cee7c86a565
rettytnova <35996553+rettytnova@users.noreply.github.com>
2024-09-27 10:07:46 +0900

    Update 덧칠하기.js

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -6,7 +6,7 @@ function solution(n, m, section) {
     // section[i]의 값에서부터 +m만큼 칠해지고 result++; -> 검색을 section[i]+m 부터 시작해야함
     let currentSection = 1; //n을 기준으로 절댓값수치로 매겨진 구역
     let i = 0; //section의 인덱스
-    while (i < section.length){
+    while (i < section.length){ //조건을 currentSection <= n 으로 하여도 상관없음.
         if(currentSection === section[i]){
             answer++;
             currentSection += m;
