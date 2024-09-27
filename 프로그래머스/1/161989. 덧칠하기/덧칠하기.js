01f6b499fa2a08ca0842f87dba702c15d462c8e9
rettytnova <35996553+rettytnova@users.noreply.github.com>
2024-09-27 10:03:01 +0900

    [level 1] Title: 덧칠하기, Time: 2.70 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,22 @@
+function solution(n, m, section) {
+    let answer = 0;
+    // n : 총량 
+    // m : 한 번 칠할 수 있는 범위
+    // section : 칠해야 하는 낱개 배열
+    // section[i]의 값에서부터 +m만큼 칠해지고 result++; -> 검색을 section[i]+m 부터 시작해야함
+    let currentSection = 1; //n을 기준으로 절댓값수치로 매겨진 구역
+    let i = 0; //section의 인덱스
+    while (currentSection <= n){
+        if(currentSection === section[i]){
+            answer++;
+            currentSection += m;
+            while(currentSection > section[i]){
+                i++;
+            }
+        }
+        else{
+            currentSection++;
+        }
+    }
+    return answer;
+}
