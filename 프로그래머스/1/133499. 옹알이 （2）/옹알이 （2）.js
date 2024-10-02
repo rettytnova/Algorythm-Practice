f4546a11125b9345bcc31638a51e46df98c3901d
rettytnova <35996553+rettytnova@users.noreply.github.com>
2024-10-02 15:52:20 +0900

    Update 옹알이 （2）.js
    필요없는 연산 및 console.log 실행문 제거

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -7,7 +7,6 @@ function solution(babbling) {
     babbling.forEach((e) => {
         let str = e;
         let lastWordIndex = -1;
-        console.log("------e:",e);
         while(str.length){
             let isCorrect = false;
             
@@ -15,7 +14,7 @@ function solution(babbling) {
                 const matchIndex = str.indexOf(words[i]);
                 
                 if(matchIndex === 0 && lastWordIndex !== i){       
-                    str = str.slice(matchIndex + words[i].length);
+                    str = str.slice(words[i].length);
                     lastWordIndex = i;
                     isCorrect = true;                    
                     break;
