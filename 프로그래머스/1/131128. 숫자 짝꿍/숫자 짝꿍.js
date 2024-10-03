de2787d9cef9673c1dca0ed118176ac0d58b5433
rettytnova <35996553+rettytnova@users.noreply.github.com>
2024-10-03 22:16:29 +0900

    [level 1] Title: 숫자 짝꿍, Time: 658.39 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,70 +1,13 @@
-
-function solution(X, Y) {
-    let answer = "";
-    // 짝꿍은 내림차순으로 정렬된 값이다.
-    // 따라서 X, Y를 내림차순으로 정렬해 인덱스0부터 비교해가며 매칭되는 갯수만큼 문자열에 추가하면
-    // 가장 큰 수를 문자열로 나타낼 수 있다.
-    const sortedX = X.split("").sort((a, b) => b - a).join("");
-    const sortedY = Y.split("").sort((a, b) => b - a).join("");
-    const maxNumberOfX = sortedX[0];
-    const maxNumberOfY = sortedY[0];
-    let presentNumber = 0;
-    let countX = 0;
-    let countY = 0;    
-    let indexX = 0;
-    let indexY = 0;
-    
-    if (maxNumberOfX < maxNumberOfY){
-        presentNumber = +maxNumberOfX;        
-    } else {
-        presentNumber = +maxNumberOfY;        
-    }
-    
-    // 아래 반복문의 탐색횟수는 X.length + Y.length + (presentNumber * 2)이다. 
-    while(presentNumber >= 0){
-        let search = true;
-        
-        while (search){
-            if (presentNumber < +sortedX[indexX]){
-                indexX++;
-            }
-            else if (presentNumber == sortedX[indexX]){
-                indexX++;
-                countX++;
-            }
-            else {
-                search = false;
-            }
-        }
-        
-        search = true;
-        
-        while (search){
-            if (presentNumber < +sortedY[indexY]){
-                indexY++;
-            }
-            else if (presentNumber == sortedY[indexY]){
-                indexY++;
-                countY++;
-            }
-            else {
-                search = false;
-            }
-        }        
-        if (countX || countY){
-            const count = (countX > countY) ? countY : countX;
-            for (let i = 0; i < count; i++){
-                answer += presentNumber;
-            }            
-            countX = 0;
-            countY = 0;
-        }
-        presentNumber--;
-    }
-    if (answer.length === 0){
-        answer += "-1";
-    } else if( answer[0] === "0"){
-        answer = "0";
-    }
-    return answer;
+const solution = (X,Y)=> {
+    const arr = Array(10).fill(0);
+    const arr2 = Array(10).fill(0);
+    [...X].forEach(i=>arr[i]++);
+    [...Y].forEach(i=>arr2[i]++);
+    let answer = []
+    arr.forEach((num,idx)=>{
+        const result = Math.min(num,arr2[idx])
+        Array(result).fill(undefined).forEach(i=>answer.push(idx))
+    })
+    answer = answer.sort((a,b)=>b-a).join('')
+    return answer[0] == 0 ? '0' : answer || '-1'
 }
