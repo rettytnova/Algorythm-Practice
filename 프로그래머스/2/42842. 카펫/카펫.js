8a9640cc45bdba37c142ae592fba2b28595568c2
rettytnova <35996553+rettytnova@users.noreply.github.com>
2024-11-11 10:27:24 +0900

    Update 카펫.js

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -2,12 +2,9 @@ function solution(brown, yellow) {
     let answer = [];    
     let width;
     let height;
-    // brown = 2x + 2y - 4
-    // yellow = (x - 2) * (y - 2) = xy - 2x - 2y + 4
-    // xy = brown + yellow
     
     const total = brown + yellow;
-    for(let i = 3; i * i <= brown + yellow; i++){
+    for(let i = 3; i * i <= total; i++){
         if(total % i){
             continue;
         }
